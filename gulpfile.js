fc7652183154d982218f35cff9bf67eec39cb4c7
gonzoibox <gonzoibox@gmail.com>
2019-04-08 15:06:11 +0300

    add slyles scss + plumber

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,12 @@ const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 const cssnano = require('gulp-cssnano');
 const browserSync = require('browser-sync');
+const plumber = require('gulp-plumber');
 
 gulp.task('scss', () => {
   return gulp
     .src('dev/scss/**/*.scss')
+    .plumber()
     .pipe(sass())
     .pipe(
       autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], {
